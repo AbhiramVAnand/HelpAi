c1181f573d74da2833e19985db5722b0b436c009
abhiram <anandabhiram0ar@gmail.com>
2024-04-09 12:14:26 +0530

    API

--- chat_server.py
+++ chat_server.py
@@ -17,7 +17,6 @@ embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 db = FAISS.from_documents(pages, embeddings)
 
 def answer_query(user_query):
-    db = current_app.db 
     qa_prompt = "Use the following pieces of context to answer the user's question. If you don't know the answer, just say that you don't know, don't try to make up an answer.----------------"
     content = "\n".join([x.page_content for x in db.similarity_search(user_query)])
     input_text = qa_prompt + "\nContext:" + content + "\nUser question:\n" + user_query
